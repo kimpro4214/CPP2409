7671e7c70f81bad6cbeafc6527b0504582842ece
kimpro4214 <qnfghk555@naver.com>
2024-09-23 14:56:42 +0900

    file folder change

---
+++ challenge/week2/temperature.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x;
+    
+    cout << "화씨온도 : " ;
+    cin >> x;
+
+    float y;
+    y = (5.0 / 9.0) * (x - 32);
+    cout << "섭씨온도 : " << y << endl;
+
+
+}
