8af8a5b677a5d2f48d70e4e03d1bc0597e3bb2eb
kimpro4214 <qnfghk555@naver.com>
2024-11-20 16:38:53 +0900

    Inheritance Exercise

---
+++ practice/week12/person_inher.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Person{
+protected: 
+    string address;
+    string name;
+};
+
+class Student : Person {
+public: 
+    void SetAddress(string add) {
+        address = add;
+    }
+    string GetAddress(){
+        return address;
+    }
+    void SetName(string name) {
+        this->name = name;
+    }
+    string GetName(){
+        return name;
+    }
+};
+
+int main() {
+    Student obj;
+    obj.SetName("미수");
+    obj.SetAddress("서울시 종로구 1번지");
+    
+    cout << obj.GetName() << endl;
+    cout << obj.GetAddress() << endl;
+
+    return 0;
+}
