f176a4fc4b038b2947be24bc091f0674eb10ba4e
kimpro4214 <qnfghk555@naver.com>
2024-11-25 19:08:45 +0900

    Modifiy Mud-game User Interface

--- project/week13_mudgame/user.cpp
+++ project/week13_mudgame/user.cpp
@@ -1,37 +1,54 @@
 #include "user.h"
+#include <iostream>
 
-User::User() {
-    hp = 20; // 초기 체력 설정
-    item = 0; // 초기 아이템 개수 설정
-}
+using namespace std;
 
-void User::DecreaseHP(int dec_hp) {
+// Magician 클래스 구현
+void Magician::DecreaseHP(int dec_hp) {
     hp -= dec_hp;
-    if (hp < 0) hp = 0; // 체력이 0 이하가 되지 않도록
+    if (hp < 0) hp = 0;
 }
 
-void User::IncreaseHP(int inc_hp) {
+void Magician::IncreaseHP(int inc_hp) {
     hp += inc_hp;
 }
 
-int User::GetHP() const {
+int Magician::GetHP() const {
     return hp;
 }
 
-int User::itemCnt() const{
+int Magician::itemCnt() const {
     return item;
 }
 
-void User::IncreaseItemCnt(int inc_item){
+void Magician::IncreaseItemCnt(int inc_item) {
     item += inc_item;
-}
- 
-void User::doAttack() const {
-    cout << "공격합니다" << endl;
 }
 
 void Magician::doAttack() const {
     cout << "마법 사용" << endl;
+}
+
+// Warrior 클래스 구현
+void Warrior::DecreaseHP(int dec_hp) {
+    hp -= dec_hp;
+    if (hp < 0) hp = 0;
+}
+
+void Warrior::IncreaseHP(int inc_hp) {
+    hp += inc_hp;
+}
+
+int Warrior::GetHP() const {
+    return hp;
+}
+
+int Warrior::itemCnt() const {
+    return item;
+}
+
+void Warrior::IncreaseItemCnt(int inc_item) {
+    item += inc_item;
 }
 
 void Warrior::doAttack() const {
--- project/week13_mudgame/user.h
+++ project/week13_mudgame/user.h
@@ -3,31 +3,52 @@ using namespace std;
 
 class User {
 private:
-    int hp; // 체력
+    int hp;
     int item;
 
 public:
-    User(); // 생성자
-    void DecreaseHP(int dec_hp); // 체력 감소 함수
-    void IncreaseHP(int inc_hp); // 체력 증가 함수
-    int GetHP() const; // 체력 반환 함수
-    int itemCnt() const; // 아이템 개수 반환 함수
-    void IncreaseItemCnt(int inc_item); // 아이템 개수 증가 함수
-    virtual void doAttack() const; // 공격 함수 
+    virtual ~User() {} // 가상 소멸자
+    virtual void DecreaseHP(int dec_hp) = 0; // 체력 감소 함수 (순수 가상 함수)
+    virtual void IncreaseHP(int inc_hp) = 0; // 체력 증가 함수 (순수 가상 함수)
+    virtual int GetHP() const = 0; // 체력 반환 함수 (순수 가상 함수)
+    virtual int itemCnt() const = 0; // 아이템 개수 반환 함수 (순수 가상 함수)
+    virtual void IncreaseItemCnt(int inc_item) = 0; // 아이템 개수 증가 함수 (순수 가상 함수)
+    virtual void doAttack() const = 0; // 공격 함수 (순수 가상 함수)
 
-   
-    friend ostream& operator<<(ostream& os, const User& itemCnt){
-        os<<"현재 체력 : " << itemCnt.hp << " , " << "획득 아이템 개수 : " << itemCnt.item << endl;
+    friend ostream& operator<<(ostream& os, const User& user) {
+        os << "현재 체력 : " << user.GetHP() << " , " << "획득 아이템 개수 : " << user.itemCnt() << endl;
         return os;
     }
 };
 
+// Magician 클래스 (User를 상속)
 class Magician : public User {
+private:
+    int hp;
+    int item;
+
 public:
-    void doAttack() const; // 공격 함수 ("마법 사용" 출력)
+    Magician() : hp(20), item(0) {} // 생성자 구현
+    void DecreaseHP(int dec_hp) override;
+    void IncreaseHP(int inc_hp) override;
+    int GetHP() const override;
+    int itemCnt() const override;
+    void IncreaseItemCnt(int inc_item) override;
+    void doAttack() const override;
 };
 
+// Warrior 클래스 (User를 상속)
 class Warrior : public User {
+private:
+    int hp;
+    int item;
+
 public:
-    void doAttack() const; // 공격 함수 ("베기 사용" 출력)
+    Warrior() : hp(20), item(0) {} // 생성자 구현
+    void DecreaseHP(int dec_hp) override;
+    void IncreaseHP(int inc_hp) override;
+    int GetHP() const override;
+    int itemCnt() const override;
+    void IncreaseItemCnt(int inc_item) override;
+    void doAttack() const override;
 };
